16ca58d4dddfd99b362f1373a3818bd7892e79fa
onatyap <oyapici17@ku.edu.tr>
2021-05-11 19:59:06 +0300

    change file read mode, optimize imports

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-import numpy as np
 import sys
-import os
+
+import numpy as np
+
 
 def floyd_warshall_algo(n, adj_matrix):  # all pairs shortest path
     adj_matrix_temp = adj_matrix.copy()
@@ -24,10 +25,10 @@ def adj_list_to_matrix(adj_list):
 
 
 def read_input(f_name):
-    with open(f_name) as data:
+    with open(f_name, 'r') as data:
         n, q = [int(x) for x in data.readline().split()]
-        adj_list = [int(x)-1 for x in data.readline().split()]
-        nearest_nodes = [[int(x)-1 for x in data.readline().split()] for _ in range(q)]
+        adj_list = [int(x) - 1 for x in data.readline().split()]
+        nearest_nodes = [[int(x) - 1 for x in data.readline().split()] for _ in range(q)]
     return n, q, adj_list, nearest_nodes
 
 
