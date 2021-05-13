e1653a30f0836858158011ff8332c9d819b25c29
taha <mehmettaha.bastug@tokeninc.com>
2021-05-13 16:02:36 +0300

    Changes by onursahin for Dijkstra and BFS

--- tests/test_floyd_warshall.py
+++ tests/test_floyd_warshall.py
@@ -56,6 +56,44 @@ class FloydWarshallTests(unittest.TestCase):
             self.assertEqual(distances[0], d0, msg='Wrong dist1')
             self.assertEqual(distances[1], d1, msg='Wrong dist2')
             self.assertEqual(distances[2], d2, msg='Wrong dist3')
+        debug_test_end()
+
+    def test_floyd_warshall_example4(self):
+        debug_test_start(problem_name, 4)
+        test_true = ['8 0 3 4',
+                     '3 1 1 1',
+                     '3 0 2 3',
+                     '8 4 1 3']
+        n, q, node_connection_list, friend_locations = read_input('example4.txt')
+        solution = self.createFloydWarshall(n, q, node_connection_list, friend_locations)
+
+        for index, (vertex, distances) in enumerate(solution.run()):
+            debug_test_output(vertex, distances)
+            v, d0, d1, d2 = [int(x) for x in test_true[index].split()]
+            self.assertEqual(vertex, v, msg='Wrong vertex')
+            self.assertEqual(distances[0], d0, msg='Wrong dist1')
+            self.assertEqual(distances[1], d1, msg='Wrong dist2')
+            self.assertEqual(distances[2], d2, msg='Wrong dist3')
+        debug_test_end()
+
+    def test_floyd_warshall_example5(self):
+        debug_test_start(problem_name, 5)
+        test_true = ['3 2 3 4',
+                     '15 2 1 2',
+                     '3 3 2 2',
+                     '3 2 2 2']
+        n, q, node_connection_list, friend_locations = read_input('example5.txt')
+        solution = self.createFloydWarshall(n, q, node_connection_list, friend_locations)
+
+        for index, (vertex, distances) in enumerate(solution.run()):
+            debug_test_output(vertex, distances)
+            v, d0, d1, d2 = [int(x) for x in test_true[index].split()]
+            self.assertEqual(vertex, v, msg='Wrong vertex')
+            self.assertEqual(distances[0], d0, msg='Wrong dist1')
+            self.assertEqual(distances[1], d1, msg='Wrong dist2')
+            self.assertEqual(distances[2], d2, msg='Wrong dist3')
+        debug_test_end()
+
 
 if __name__ == '__main__':
     unittest.main()
